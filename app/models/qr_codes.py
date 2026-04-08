d48774d1bc353bd96ff51588f165471af8ce496f
Sardorbek <nsardorbek776@gmail.com>
2026-04-08 14:53:18 +0500

    update

--- app/models/qr_codes.py
+++ app/models/qr_codes.py
@@ -1,10 +1,9 @@
 from uuid import uuid4
 from django.db import models
-from .log_connection import SyncSoftDeleteMixin
 from django.utils.translation import gettext_lazy as _
 
 
-class QRCode(SyncSoftDeleteMixin):
+class QRCode(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     unique_code = models.CharField(max_length=255, unique=True)
     image = models.ImageField(upload_to='qr_codes/')
