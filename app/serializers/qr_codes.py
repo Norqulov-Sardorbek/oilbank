4e90926d6da61aa361ce8a2f5f86b534a2d120d9
Sardorbek <nsardorbek776@gmail.com>
2026-04-08 17:51:03 +0500

    update

--- app/serializers/qr_codes.py
+++ app/serializers/qr_codes.py
@@ -4,6 +4,8 @@ from io import BytesIO
 import qrcode
 from django.core.files.base import ContentFile
 from rest_framework import serializers
+from qrcode.constants import ERROR_CORRECT_H
+from PIL import Image
 
 from app.models.qr_codes import QRCode
 
@@ -23,12 +25,45 @@ class QRCodeSerializer(serializers.ModelSerializer):
                 return code
 
     def generate_qr_image(self, unique_code: str) -> ContentFile:
-        qr = qrcode.make(unique_code)
+        qr = qrcode.QRCode(
+        version=None,
+        error_correction=ERROR_CORRECT_H,
+        box_size=10,
+        border=1,
+    )
+        qr.add_data(unique_code)
+        qr.make(fit=True)
 
+        qr_img = qr.make_image(
+            fill_color="black",
+            back_color="white"
+        ).convert("RGBA")
+
+        # 2. Frame (ramka) ni ochamiz
+        frame = Image.open("utils/Carland_Sticker_Glass_V1_Монтажная область 1.tif").convert("RGBA")
+        # 🔁 o‘zingni path qo‘y: masalan media yoki static
+
+        # 3. QR size ni framega moslab kichraytiramiz
+        frame_w, frame_h = frame.size
+
+        qr_size = int(frame_w * 0.6)  # QR frame ichida 50% bo‘lsin
+        qr_img = qr_img.resize((qr_size, qr_size))
+
+        # 4. O‘rtaga joylaymiz
+        pos = (
+            (frame_w - qr_size) // 2+8,
+            (frame_h - qr_size) // 2-130,
+        )
+
+        # 5. QR ni frame ustiga qo‘yish
+        frame.paste(qr_img, pos, qr_img)
+
+        # 6. Saqlash
+        
         buffer = BytesIO()
-        qr.save(buffer, format='PNG')
-
-        filename = f'{unique_code}.png'
+        
+        frame.save(buffer, format="PNG")
+        filename = f"{unique_code}.png"
         return ContentFile(buffer.getvalue(), name=filename)
 
     def create(self, validated_data):
