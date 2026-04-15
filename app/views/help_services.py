dd38b1fa7bc367482d5b8eddf9ae83e149807996
Sardorbek <nsardorbek776@gmail.com>
2026-04-15 15:12:07 +0500

    update

--- app/views/help_services.py
+++ app/views/help_services.py
@@ -1,17 +1,16 @@
-from rest_framework import status, viewsets
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from app.serializers.help_services import HelpServiceSerializer,HelpServiceEmployeeSerializer
 from app.models.help_services import HelpService, HelpServiceEmployee
 
 
 class HelpServiceViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     queryset = HelpService.objects.all()
     serializer_class = HelpServiceSerializer
     
 
 class HelpServiceEmployeeViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [  AllowAny]
     queryset = HelpServiceEmployee.objects.all()
     serializer_class = HelpServiceEmployeeSerializer
